8c29838dfad3d98e23848200d09bb749b8ca7b5e
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-01-04 08:47:22 +0100

    small fixup

--- src/Bechtle.A365.ConfigService/Controllers/PreviewController.cs
+++ src/Bechtle.A365.ConfigService/Controllers/PreviewController.cs
@@ -4,7 +4,6 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Bechtle.A365.ConfigService.Common.Compilation;
-using Bechtle.A365.ConfigService.Common.Compilation.Introspection.Results;
 using Bechtle.A365.ConfigService.Common.Converters;
 using Bechtle.A365.ConfigService.Common.DomainEvents;
 using Bechtle.A365.ConfigService.Parsing;
