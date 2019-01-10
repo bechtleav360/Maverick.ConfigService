4df43ca8c8702f0abbc84cdd3fb9a33f5285b23e
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-01-10 09:55:05 +0100

    add action to view complete history of environment

--- src/Bechtle.A365.ConfigService/Controllers/HistoryController.cs
+++ src/Bechtle.A365.ConfigService/Controllers/HistoryController.cs
@@ -32,16 +32,14 @@ namespace Bechtle.A365.ConfigService.Controllers
         /// </summary>
         /// <param name="category"></param>
         /// <param name="name"></param>
-        /// <param name="when"></param>
         /// <returns></returns>
         [HttpGet("blame/environment/{category}/{name}")]
         public async Task<IActionResult> BlameEnvironment([FromRoute] string category,
-                                                          [FromRoute] string name,
-                                                          [FromQuery] DateTime when)
+                                                          [FromRoute] string name)
         {
             var events = (await _eventStore.ReplayEvents()).ToArray();
 
-            var blameData = new Dictionary<string, KeyBlameData>();
+            var blameData = new Dictionary<string, KeyRevision>();
 
             foreach (var (recordedEvent, domainEvent) in events)
             {
@@ -58,7 +56,7 @@ namespace Bechtle.A365.ConfigService.Controllers
                     switch (action.Type)
                     {
                         case ConfigKeyActionType.Set:
-                            blameData[action.Key] = new KeyBlameData
+                            blameData[action.Key] = new KeyRevision
                             {
                                 DateTime = recordedEvent.Created,
                                 Value = action.Value
@@ -79,12 +77,84 @@ namespace Bechtle.A365.ConfigService.Controllers
             return Ok(blameData);
         }
 
-        // TODO: find a better name?
-        private class KeyBlameData
+        /// <summary>
+        ///     get the complete history and metadata of an environment
+        /// </summary>
+        /// <param name="category"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("environment/{category}/{name}")]
+        public async Task<IActionResult> GetEnvironmentHistory([FromRoute] string category,
+                                                               [FromRoute] string name)
+        {
+            var events = (await _eventStore.ReplayEvents()).ToArray();
+
+            var history = new Dictionary<string, KeyHistory>();
+
+            foreach (var (recordedEvent, domainEvent) in events)
+            {
+                // we only care about events that modify environment-keys
+                if (!(domainEvent is EnvironmentKeysModified keysModified))
+                    continue;
+
+                if (!keysModified.Identifier.Category.Equals(category, StringComparison.InvariantCultureIgnoreCase) ||
+                    !keysModified.Identifier.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase))
+                    continue;
+
+                foreach (var action in keysModified.ModifiedKeys)
+                {
+                    if (!history.ContainsKey(action.Key))
+                        history[action.Key] = new KeyHistory
+                        {
+                            Key = action.Key,
+                            History = new SortedList<DateTime, KeyRevision>()
+                        };
+
+                    switch (action.Type)
+                    {
+                        case ConfigKeyActionType.Set:
+                            history[action.Key].History.Add(recordedEvent.Created, new KeyRevision
+                            {
+                                Type = ConfigKeyActionType.Set,
+                                DateTime = recordedEvent.Created,
+                                Value = action.Value
+                            });
+                            break;
+
+                        case ConfigKeyActionType.Delete:
+                            history[action.Key].History.Add(recordedEvent.Created, new KeyRevision
+                            {
+                                Type = ConfigKeyActionType.Delete,
+                                DateTime = recordedEvent.Created,
+                                Value = action.Value
+                            });
+                            break;
+
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+
+            return Ok(history.Values.OrderBy(r => r.Key));
+        }
+
+        // ReSharper disable UnusedAutoPropertyAccessor.Local
+        private class KeyRevision
         {
             public DateTime DateTime { get; set; }
 
             public string Value { get; set; }
+
+            public ConfigKeyActionType Type { get; set; }
         }
+
+        private class KeyHistory
+        {
+            public string Key { get; set; }
+
+            public SortedList<DateTime, KeyRevision> History { get; set; }
+        }
+        // ReSharper restore UnusedAutoPropertyAccessor.Local
     }
 }
