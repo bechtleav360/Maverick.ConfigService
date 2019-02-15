94676ce2d688946b284a1e231126f9f51aec51a3
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-02-15 15:41:38 +0100

    add installer projects

---
+++ installers/Bechtle.A365.ConfigService.Installer/SetAppPoolNoManagedCode.ps1
@@ -0,0 +1,9 @@
+﻿param([string]$appPool = "")
+
+if ($appPool -eq "") {
+    Write-Error "application pool undefined, use '-appPool SomeApplicationPool' when running this script";
+    exit 1;
+}
+
+Import-Module WebAdministration;
+Set-ItemProperty IIS:\AppPools\$appPool managedRuntimeVersion "";
---
+++ installers/Bechtle.A365.ConfigService.Installer/SetBindingCertificate.ps1
@@ -0,0 +1,27 @@
+﻿param(
+    [string] $cert = "",
+    [string] $hostname = "",
+    [string] $certstore = "MY",
+    [string] $site = ""
+);
+
+if ($cert -eq "") {
+    Write-Error "cert undefined, can't set certificate for binding";
+    exit 1;
+}
+
+if ($hostname -eq "") {
+    Write-Error "hostname undefined, can't set certificate for binding";
+    exit 1;
+}
+
+if ($site -eq ""){
+    Write-Error "site undefined, can't set certificate for binding";
+    exit 1;
+}
+
+Import-Module WebAdministration;
+
+$guid = [guid]::NewGuid().ToString("B")
+netsh http add sslcert hostnameport="${hostname}:443" certhash=$cert certstorename=$certstore appid="$guid"
+New-WebBinding -name $site -Protocol https -HostHeader $hostname -Port 443 -SslFlags 1
