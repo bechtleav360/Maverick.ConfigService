0143b8bfaa95536292f7442a59f40684420e8a1b
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-03-19 15:35:01 +0100

    move common functionality into internal functions

--- src/Bechtle.A365.ConfigService.Projection/Program.cs
+++ src/Bechtle.A365.ConfigService.Projection/Program.cs
@@ -19,61 +19,33 @@ using NLog.Web;
 
 namespace Bechtle.A365.ConfigService.Projection
 {
-    public class Program
+    public static class Program
     {
-        // entry point for application when designing migrations
         // ReSharper disable once UnusedMember.Global
         //
         // Configure and ConfigureServices are both required, either as Fluent invocations here or as actual methods in this class - even if they're empty.
         // they're both called here to not clutter this class with empty functions
+        /// <summary>
+        ///     Mock Entry-Point for building / managing Migrations
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IWebHost BuildWebHost(string[] args)
             => WebHost.CreateDefaultBuilder(args)
                       .Configure(builder => { }) // don't delete, see comment above
-                      .ConfigureAppConfiguration((context, builder) =>
-                      {
-                          builder.AddJsonFile("appsettings.json", true, true)
-                                 .AddCommandLine(args)
-                                 .AddEnvironmentVariables();
-                      })
+                      .ConfigureAppConfiguration((context, builder) => ConfigureAppConfigurationInternal(builder,
+                                                                                                         context.HostingEnvironment.EnvironmentName,
+                                                                                                         args))
                       .ConfigureServices((context, services) => ConfigureServicesInternal(services, context.Configuration))
-                      .ConfigureLogging((context, builder) =>
-                      {
-                          // set the global NLog configuration
-                          using (var stringReader = new StringReader(context.Configuration
-                                                                            .Get<ProjectionConfiguration>()
-                                                                            .LoggingConfiguration))
-                          using (var xmlReader = XmlReader.Create(stringReader))
-                              LogManager.Configuration = new XmlLoggingConfiguration(xmlReader, null);
-
-                          builder.ClearProviders();
-                      })
+                      .ConfigureLogging((context, builder) => ConfigureLoggingInternal(builder, context.Configuration))
                       .UseNLog()
                       .Build();
 
-        private static IHostBuilder BuildHost(string[] args)
-            => new HostBuilder()
-               .ConfigureAppConfiguration((context, builder) =>
-               {
-                   builder.AddJsonFile("appsettings.json", true, true)
-                          .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true)
-                          .AddCommandLine(args)
-                          .AddEnvironmentVariables();
-               })
-               .ConfigureServices((context, services) => ConfigureServicesInternal(services, context.Configuration))
-               .ConfigureLogging((context, builder) =>
-               {
-                   // set the global NLog configuration
-                   using (var stringReader = new StringReader(context.Configuration
-                                                                     .Get<ProjectionConfiguration>()
-                                                                     .LoggingConfiguration))
-                   using (var xmlReader = XmlReader.Create(stringReader))
-                       LogManager.Configuration = new XmlLoggingConfiguration(xmlReader, null);
-
-                   builder.ClearProviders();
-               })
-               .UseNLog();
-
-        // actual entry-point for the application
+        /// <summary>
+        ///     actual entry-point for this Application
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static async Task Main(string[] args)
         {
             var host = BuildHost(args);
@@ -84,6 +56,52 @@ namespace Bechtle.A365.ConfigService.Projection
                 await host.RunConsoleAsync();
         }
 
+        /// <summary>
+        ///     Build the Actual host that is used to run this Application.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static IHostBuilder BuildHost(string[] args)
+            => new HostBuilder()
+               .ConfigureAppConfiguration((context, builder) => ConfigureAppConfigurationInternal(builder,
+                                                                                                  context.HostingEnvironment.EnvironmentName,
+                                                                                                  args))
+               .ConfigureServices((context, services) => ConfigureServicesInternal(services, context.Configuration))
+               .ConfigureLogging((context, builder) => ConfigureLoggingInternal(builder, context.Configuration))
+               .UseNLog();
+
+        /// <summary>
+        ///     Configure Application-Configuration
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="environment"></param>
+        /// <param name="args"></param>
+        private static void ConfigureAppConfigurationInternal(IConfigurationBuilder builder, string environment, string[] args)
+            => builder.AddJsonFile("appsettings.json", true, true)
+                      .AddJsonFile($"appsettings.{environment}.json", true, true)
+                      .AddCommandLine(args)
+                      .AddEnvironmentVariables();
+
+        /// <summary>
+        ///     Configure Application-Logging
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="configuration"></param>
+        private static void ConfigureLoggingInternal(ILoggingBuilder builder, IConfiguration configuration)
+        {
+            // set the global NLog configuration
+            using (var stringReader = new StringReader(configuration.Get<ProjectionConfiguration>().LoggingConfiguration))
+            using (var xmlReader = XmlReader.Create(stringReader))
+                LogManager.Configuration = new XmlLoggingConfiguration(xmlReader, null);
+
+            builder.ClearProviders();
+        }
+
+        /// <summary>
+        ///     Configure DI-Services
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
         private static void ConfigureServicesInternal(IServiceCollection services, IConfiguration configuration)
             => services.AddDbContext<ProjectionStoreContext>(
                            (provider, builder)
