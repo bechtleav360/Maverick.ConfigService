8ed2ee55787e58a9d40a76b278519d907c623c63
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-03-28 14:28:18 +0100

    update BindingCertificate-Script to newest version

--- installers/Bechtle.A365.ConfigService.Installer/SetBindingCertificate.ps1
+++ installers/Bechtle.A365.ConfigService.Installer/SetBindingCertificate.ps1
@@ -1,27 +1,41 @@
 ﻿param(
-    [string] $cert = "",
+    [string] $thumbprint = "",
+    [string] $certname = "",
     [string] $hostname = "",
     [string] $certstore = "MY",
     [string] $site = ""
 );
 
-if ($cert -eq "") {
-    Write-Error "cert undefined, can't set certificate for binding";
+New-EventLog -LogName Application -Source "A365Setup" -ErrorAction Ignore
+
+if([String]::IsNullOrWhiteSpace($thumbprint) -and [String]::IsNullOrWhiteSpace($certname)) {
+    Write-Error -Message "Certname and thumbprint empty"
+    Write-EventLog -LogName Application -Source "A365Setup" -EntryType Information -EventId 1 -Message "Certname and thumbprint empty"
     exit 1;
 }
 
-if ($hostname -eq "") {
-    Write-Error "hostname undefined, can't set certificate for binding";
+if([String]::IsNullOrWhiteSpace($thumbprint)) {
+    $thumbprint = (Get-ChildItem -Path Cert:\LocalMachine\$certstore | Where-Object {$_.FriendlyName -match $certname}).Thumbprint;
+
+    if([String]::IsNullOrWhiteSpace($thumbprint)) {
+        Write-EventLog -LogName Application -Source "A365Setup" -EntryType Information -EventId 1 -Message "Failed to get thumbprint of Cert '$certname'";
+        exit 1;
+    }
+}
+
+if([String]::IsNullOrWhiteSpace($hostname)) {
+    Write-EventLog -LogName Application -Source "A365Setup" -EntryType Information -EventId 1 -Message "hostname undefined, can't set certificate for binding";
     exit 1;
 }
 
-if ($site -eq ""){
-    Write-Error "site undefined, can't set certificate for binding";
+if([String]::IsNullOrWhiteSpace($site)) {
+    Write-EventLog -LogName Application -Source "A365Setup" -EntryType Information -EventId 1 -Message "site undefined, can't set certificate for binding";
     exit 1;
 }
 
 Import-Module WebAdministration;
 
-$guid = [guid]::NewGuid().ToString("B")
-netsh http add sslcert hostnameport="${hostname}:443" certhash=$cert certstorename=$certstore appid="$guid"
+$guid = [Guid]::NewGuid().ToString("B")
+netsh http add sslcert hostnameport="${hostname}:443" certhash=$thumbprint certstorename=$certstore appid="$guid"
+
 New-WebBinding -name $site -Protocol https -HostHeader $hostname -Port 443 -SslFlags 1
