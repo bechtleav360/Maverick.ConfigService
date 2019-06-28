0919ee691307ef34c94ba113e476e39ff7eb05e8
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-06-28 15:47:38 +0200

    remove WebHost building from Projection - now in Cli

--- src/Bechtle.A365.ConfigService.Projection/Program.cs
+++ src/Bechtle.A365.ConfigService.Projection/Program.cs
@@ -7,40 +7,18 @@ using Bechtle.A365.ConfigService.Common.DbObjects;
 using Bechtle.A365.ConfigService.Common.Utilities;
 using Bechtle.A365.ConfigService.Configuration;
 using Bechtle.A365.ConfigService.Projection.Extensions;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
-using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace Bechtle.A365.ConfigService.Projection
 {
     public static class Program
     {
         public static readonly CancellationTokenSource CancellationTokenSource = new CancellationTokenSource();
-
-        // ReSharper disable once UnusedMember.Global
-        //
-        // Configure and ConfigureServices are both required, either as Fluent invocations here or as actual methods in this class - even if they're empty.
-        // they're both called here to not clutter this class with empty functions
-        /// <summary>
-        ///     Mock Entry-Point for building / managing Migrations
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static IWebHost BuildWebHost(string[] args)
-            => WebHost.CreateDefaultBuilder(args)
-                      .Configure(builder => { }) // don't delete, see comment above
-                      .ConfigureAppConfiguration((context, builder) => ConfigureAppConfigurationInternal(builder,
-                                                                                                         context.HostingEnvironment.EnvironmentName,
-                                                                                                         args))
-                      .ConfigureServices((context, services) => ConfigureServicesInternal(services, context.Configuration))
-                      .ConfigureLogging((context, builder) => ConfigureLoggingInternal(builder, context.Configuration))
-                      .Build();
 
         /// <summary>
         ///     actual entry-point for this Application
@@ -75,9 +53,7 @@ namespace Bechtle.A365.ConfigService.Projection
         private static IHostBuilder BuildHost(string[] args)
             => new HostBuilder()
                .ConfigureHostConfiguration(builder => builder.AddEnvironmentVariables("ASPNETCORE_"))
-               .ConfigureAppConfiguration((context, builder) => ConfigureAppConfigurationInternal(builder,
-                                                                                                  context.HostingEnvironment.EnvironmentName,
-                                                                                                  args))
+               .ConfigureAppConfiguration((context, builder) => ConfigureAppConfigurationInternal(builder, context, args))
                .ConfigureServices((context, services) => ConfigureServicesInternal(services, context.Configuration))
                .ConfigureLogging((context, builder) => ConfigureLoggingInternal(builder, context.Configuration));
 
@@ -85,11 +61,11 @@ namespace Bechtle.A365.ConfigService.Projection
         ///     Configure Application-Configuration
         /// </summary>
         /// <param name="builder"></param>
-        /// <param name="environment"></param>
+        /// <param name="context"></param>
         /// <param name="args"></param>
-        private static void ConfigureAppConfigurationInternal(IConfigurationBuilder builder, string environment, string[] args)
+        private static void ConfigureAppConfigurationInternal(IConfigurationBuilder builder, HostBuilderContext context, string[] args)
             => builder.AddJsonFile("appsettings.json", true, true)
-                      .AddJsonFile($"appsettings.{environment}.json", true, true)
+                      .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true)
                       .AddEnvironmentVariables()
                       .AddEnvironmentVariables("MAV_CONFIG_PROJECTION")
                       .AddCommandLine(args);
