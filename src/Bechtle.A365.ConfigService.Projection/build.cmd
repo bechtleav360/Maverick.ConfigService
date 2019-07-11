5da71924d53854ecdd6424acc15e58861ffa0d09
Michael Trinkies <michael.trinkies@bechtle.com>
2019-07-11 11:15:58 +0200

    added docker compatibility

---
+++ src/Bechtle.A365.ConfigService.Projection/build.cmd
@@ -0,0 +1,4 @@
+﻿@echo off
+dotnet publish -c Release -r debian.9-x64
+docker build -t localhost:5000/configservice_projection bin/Release/netcoreapp2.2/debian.9-x64/publish/
+docker-compose up -d --force-recreate
