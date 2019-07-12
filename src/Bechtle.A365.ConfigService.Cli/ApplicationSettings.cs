03fba7308f1d7327864c26dfe4c9a9c67a4a0293
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-07-12 10:51:58 +0200

    move DbBackend into its own files

--- src/Bechtle.A365.ConfigService.Cli/ApplicationSettings.cs
+++ src/Bechtle.A365.ConfigService.Cli/ApplicationSettings.cs
@@ -10,20 +10,4 @@ namespace Bechtle.A365.ConfigService.Cli
 
         public IConfiguration EffectiveConfiguration { get; set; }
     }
-
-    /// <summary>
-    ///     The Database-Backend to use
-    /// </summary>
-    public enum DbBackend
-    {
-        /// <summary>
-        ///     Use Microsofts SQL-Server
-        /// </summary>
-        MsSql,
-
-        /// <summary>
-        ///     Use the Open-Source Database PostgreSql
-        /// </summary>
-        Postgres
-    }
 }
