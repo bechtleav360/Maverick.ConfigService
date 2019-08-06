c09eb494840cf42bb4b3d46bbd39cf02da3c73d2
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-08-06 16:14:13 +0200

    add endpoint to see which env-keys are unused

--- src/Bechtle.A365.ConfigService/Controllers/InspectionController.cs
+++ src/Bechtle.A365.ConfigService/Controllers/InspectionController.cs
@@ -24,8 +24,8 @@ namespace Bechtle.A365.ConfigService.Controllers
     {
         private readonly IConfigurationCompiler _compiler;
         private readonly IConfigurationParser _parser;
-        private readonly IJsonTranslator _translator;
         private readonly IProjectionStore _store;
+        private readonly IJsonTranslator _translator;
 
         /// <inheritdoc />
         public InspectionController(IServiceProvider provider,
@@ -43,13 +43,78 @@ namespace Bechtle.A365.ConfigService.Controllers
         }
 
         /// <summary>
+        ///     use the most up-to-date structures to find unused keys in an environment
+        /// </summary>
+        /// <param name="environmentCategory"></param>
+        /// <param name="environmentName"></param>
+        /// <returns></returns>
+        [HttpPost("environment/{environmentCategory}/{environmentName}/structures/latest")]
+        public async Task<IActionResult> FindUnusedEnvironmentKeys([FromRoute] string environmentCategory,
+                                                                   [FromRoute] string environmentName)
+        {
+            if (string.IsNullOrWhiteSpace(environmentCategory))
+                return BadRequest("no environment-category given");
+
+            if (string.IsNullOrWhiteSpace(environmentName))
+                return BadRequest("no environment-name given");
+
+            var envId = new EnvironmentIdentifier(environmentCategory, environmentName);
+
+            var envKeyResult = await _store.Environments.GetKeys(envId, QueryRange.All);
+
+            if (envKeyResult.IsError)
+                return ProviderError(envKeyResult);
+
+            var envKeys = envKeyResult.Data;
+
+            var configResult = await _store.Configurations.GetAvailableWithEnvironment(envId, DateTime.MinValue, QueryRange.All);
+
+            if (configResult.IsError)
+                return ProviderError(configResult);
+
+            var configIds = configResult.Data
+                                        .GroupBy(c => c.Structure.Name)
+                                        .Select(g => g.OrderByDescending(c => c.Structure.Version).First())
+                                        .ToList();
+
+            var tasks = configIds.AsParallel()
+                                 .Select(cid => _store.Configurations.GetUsedConfigurationKeys(cid, DateTime.MinValue, QueryRange.All))
+                                 .ToList();
+
+            await Task.WhenAll(tasks);
+
+            var results = tasks.Select(t => t.Result).ToList();
+            var errors = results.Where(r => r.IsError).ToList();
+
+            if (errors.Any(r => r.IsError))
+                return ProviderError(errors.First());
+
+            // select all used keys from all queried configurations into a flat list
+            var usedKeys = results.SelectMany(r => r.Data)
+                                  .GroupBy(p => p)
+                                  .Select(g => g.Key)
+                                  .ToList();
+
+            var unusedKeys = new Dictionary<string, string>();
+            foreach (var (key, value) in envKeys)
+            {
+                if (usedKeys.Any(k => k.Equals(key, StringComparison.OrdinalIgnoreCase)))
+                    continue;
+
+                unusedKeys.Add(key, value);
+            }
+
+            return Ok(_translator.ToJson(unusedKeys));
+        }
+
+        /// <summary>
         ///     inspect the given structure for possible errors or unreachable references
         /// </summary>
         /// <param name="structure"></param>
         /// <param name="environmentCategory"></param>
         /// <param name="environmentName"></param>
         /// <returns></returns>
-        [HttpPost("structure/{environmentCategory}/{environmentName}")]
+        [HttpPost("structure/compile/{environmentCategory}/{environmentName}")]
         public async Task<IActionResult> InspectStructure([FromRoute] string environmentCategory,
                                                           [FromRoute] string environmentName,
                                                           [FromBody] DtoStructure structure)
@@ -73,7 +138,7 @@ namespace Bechtle.A365.ConfigService.Controllers
             if (envKeysResult.IsError)
                 return ProviderError(envKeysResult);
 
-            var envKeys = envKeysResult.Data.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            var envKeys = envKeysResult.Data;
 
             IDictionary<string, string> structKeys;
 
@@ -112,6 +177,13 @@ namespace Bechtle.A365.ConfigService.Controllers
                 });
             }
 
+            var result = AnalyzeCompilation(compilationResult);
+
+            return Ok(result);
+        }
+
+        private StructureInspectionResult AnalyzeCompilation(CompilationResult compilationResult)
+        {
             var traceList = new List<TraceResult>(compilationResult.CompiledConfiguration.Count);
             var stack = new Stack<TraceResult>();
             // prepare stack with initial data
@@ -126,24 +198,20 @@ namespace Bechtle.A365.ConfigService.Controllers
 
             var warnings = new Dictionary<string, List<string>>();
             foreach (var traceResult in traceList.OfType<KeyTraceResult>())
+            foreach (var warning in traceResult.Warnings)
             {
-                foreach (var warning in traceResult.Warnings)
-                {
-                    if (!warnings.ContainsKey(traceResult.Key))
-                        warnings.Add(traceResult.Key, new List<string>());
-                    warnings[traceResult.Key].Add(warning);
-                }
+                if (!warnings.ContainsKey(traceResult.Key))
+                    warnings.Add(traceResult.Key, new List<string>());
+                warnings[traceResult.Key].Add(warning);
             }
 
             var errors = new Dictionary<string, List<string>>();
             foreach (var traceResult in traceList.OfType<KeyTraceResult>())
+            foreach (var error in traceResult.Errors)
             {
-                foreach (var error in traceResult.Errors)
-                {
-                    if (!errors.ContainsKey(traceResult.Key))
-                        errors.Add(traceResult.Key, new List<string>());
-                    errors[traceResult.Key].Add(error);
-                }
+                if (!errors.ContainsKey(traceResult.Key))
+                    errors.Add(traceResult.Key, new List<string>());
+                errors[traceResult.Key].Add(error);
             }
 
             var result = new StructureInspectionResult
@@ -159,7 +227,7 @@ namespace Bechtle.A365.ConfigService.Controllers
                 Errors = errors
             };
 
-            return Ok(result);
+            return result;
         }
     }
 
@@ -169,27 +237,27 @@ namespace Bechtle.A365.ConfigService.Controllers
     public class StructureInspectionResult
     {
         /// <summary>
+        ///     flag indicating if the compilation was successful or not
+        /// </summary>
+        public bool CompilationSuccessful { get; set; }
+
+        /// <summary>
         ///     resulting compiled configuration
         /// </summary>
         public IDictionary<string, string> CompiledConfiguration { get; set; } = new Dictionary<string, string>();
 
         /// <summary>
-        ///     flag indicating if the compilation was successful or not
+        ///     Path => Error dictionary
         /// </summary>
-        public bool CompilationSuccessful { get; set; } = false;
+        public Dictionary<string, List<string>> Errors { get; set; }
 
-        /// <inheritdoc cref="CompilationStats"/>
+        /// <inheritdoc cref="CompilationStats" />
         public CompilationStats Stats { get; set; } = new CompilationStats();
 
         /// <summary>
         ///     Path => Warning dictionary
         /// </summary>
         public Dictionary<string, List<string>> Warnings { get; set; }
-
-        /// <summary>
-        ///     Path => Error dictionary
-        /// </summary>
-        public Dictionary<string, List<string>> Errors { get; set; }
     }
 
     /// <summary>
@@ -200,11 +268,11 @@ namespace Bechtle.A365.ConfigService.Controllers
         /// <summary>
         ///     number of used References
         /// </summary>
-        public int ReferencesUsed { get; set; } = 0;
+        public int ReferencesUsed { get; set; }
 
         /// <summary>
         ///     number of used Static values
         /// </summary>
-        public int StaticValuesUsed { get; set; } = 0;
+        public int StaticValuesUsed { get; set; }
     }
 }
