6492776f0fbab65ff00d1caf07dba0b704fdbd68
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-08-08 10:42:16 +0200

    fix xml-doc error

--- src/Bechtle.A365.ConfigService.Common/Utilities/CacheUtilities.cs
+++ src/Bechtle.A365.ConfigService.Common/Utilities/CacheUtilities.cs
@@ -41,17 +41,17 @@ namespace Bechtle.A365.ConfigService.Common.Utilities
     public enum CacheDuration
     {
         /// <summary>
-        ///     item should be cached for a few subsequent requests (<5s)
+        ///     item should be cached for a few subsequent requests (up to 5s)
         /// </summary>
         Tiny,
 
         /// <summary>
-        ///     item should be cached for a few requests that may be up to a minute apart (<1m)
+        ///     item should be cached for a few requests that may be up to a minute apart (up to 1m)
         /// </summary>
         Short,
 
         /// <summary>
-        ///     item should be cached for a several requests across different actions (<4m)
+        ///     item should be cached for a several requests across different actions (up to 4m)
         /// </summary>
         Medium
     }
