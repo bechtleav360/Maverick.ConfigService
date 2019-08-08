379c58ac8b086a70039beca332c69fc16ac4d568
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-08-08 10:53:19 +0200

    move CacheDuration enum to own file

--- src/Bechtle.A365.ConfigService.Common/Utilities/CacheUtilities.cs
+++ src/Bechtle.A365.ConfigService.Common/Utilities/CacheUtilities.cs
@@ -34,25 +34,4 @@ namespace Bechtle.A365.ConfigService.Common.Utilities
 
         public static string MakeCacheKey(params object[] items) => $"Mav.CS:{string.Join("", items.Select(i => i?.ToString() ?? string.Empty))}";
     }
-
-    /// <summary>
-    ///     General length of how long an object should be cached
-    /// </summary>
-    public enum CacheDuration
-    {
-        /// <summary>
-        ///     item should be cached for a few subsequent requests (up to 5s)
-        /// </summary>
-        Tiny,
-
-        /// <summary>
-        ///     item should be cached for a few requests that may be up to a minute apart (up to 1m)
-        /// </summary>
-        Short,
-
-        /// <summary>
-        ///     item should be cached for a several requests across different actions (up to 4m)
-        /// </summary>
-        Medium
-    }
 }
