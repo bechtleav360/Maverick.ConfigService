cf1c0ef2349905a530da112ccc5fce6f0f9069eb
Michael Trinkies <michael.trinkies@bechtle.com>
2019-08-12 10:25:50 +0200

    fix

--- build.cmd
+++ build.cmd
@@ -1,12 +1,15 @@
 ﻿@echo off
 
+dotnet build src/Bechtle.A365.ConfigService.Cli/Bechtle.A365.ConfigService.Cli.csproj
 dotnet publish src/Bechtle.A365.ConfigService.Cli/Bechtle.A365.ConfigService.Cli.csproj -c Release -r debian.9-x64
 docker build -t localhost:5000/configservice_migrate src/Bechtle.A365.ConfigService.Cli/bin/Release/netcoreapp2.1/debian.9-x64/publish/
 
+dotnet build src/Bechtle.A365.ConfigService.Projection/Bechtle.A365.ConfigService.Projection.csproj
 dotnet publish src/Bechtle.A365.ConfigService.Projection/Bechtle.A365.ConfigService.Projection.csproj -c Release -r debian.9-x64
 docker build -t localhost:5000/configservice_projection src/Bechtle.A365.ConfigService.Projection/bin/Release/netcoreapp2.1/debian.9-x64/publish/
 
+dotnet build src/Bechtle.A365.ConfigService/Bechtle.A365.ConfigService.csproj
 dotnet publish src/Bechtle.A365.ConfigService/Bechtle.A365.ConfigService.csproj -c Release -r debian.9-x64
 docker build -t localhost:5000/configservice src/Bechtle.A365.ConfigService/bin/Release/netcoreapp2.1/debian.9-x64/publish/
 
-docker-compose -f docker-compose.local.yml up -d
+docker-compose -f D:/TFS/Maverick/docker-compose.yml up -d configservice_init
---
+++ src/Bechtle.A365.ConfigService/healthcheck.sh
@@ -0,0 +1,9 @@
+EXPECTED="{\"category\":\"av360\",\"name\":\"dev\"}"
+RESP=$(curl -X GET "http://configservice:8082/v1/environments/available?offset=-1&length=-1" | jq -r -c '.[] | select(.category == "av360") | select(.name == "dev")')
+echo resp: "$RESP"
+echo expc: "$EXPECTED"
+if [ "$EXPECTED" = "$EXPECTED" ]; then
+echo env exists
+else
+exit 1
+fi
