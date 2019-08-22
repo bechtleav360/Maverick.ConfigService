f2b8e570c509c4d500a0100c76c8df1fcf1f031a
Michael Trinkies <michael.trinkies@bechtle.com>
2019-08-22 12:17:43 +0200

    fixed healthcheck

--- src/Bechtle.A365.ConfigService/healthcheck.sh
+++ src/Bechtle.A365.ConfigService/healthcheck.sh
@@ -2,7 +2,7 @@ EXPECTED="{\"category\":\"av360\",\"name\":\"dev\"}"
 RESP=$(curl -X GET "http://configservice:8082/v1/environments/available?offset=-1&length=-1" | jq -r -c '.[] | select(.category == "av360") | select(.name == "dev")')
 echo resp: "$RESP"
 echo expc: "$EXPECTED"
-if [ "$EXPECTED" = "$EXPECTED" ]; then
+if [ "$RESP" = "$EXPECTED" ]; then
 echo env exists
 else
 exit 1
