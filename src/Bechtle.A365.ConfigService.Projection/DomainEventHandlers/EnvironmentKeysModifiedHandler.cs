3b26dd4e3d6aaa48685c34823068cfcb45b4d8bb
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-09-27 15:11:04 +0200

    change behaviour for failed DB-Updates

--- src/Bechtle.A365.ConfigService.Projection/DomainEventHandlers/EnvironmentKeysModifiedHandler.cs
+++ src/Bechtle.A365.ConfigService.Projection/DomainEventHandlers/EnvironmentKeysModifiedHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Bechtle.A365.ConfigService.Common;
 using Bechtle.A365.ConfigService.Common.DomainEvents;
@@ -23,17 +24,26 @@ namespace Bechtle.A365.ConfigService.Projection.DomainEventHandlers
         /// <inheritdoc />
         public async Task HandleDomainEvent(EnvironmentKeysModified domainEvent)
         {
+            if (domainEvent is null)
+                throw new ArgumentNullException(nameof(domainEvent), $"{nameof(domainEvent)} must not be null");
+
             var setKeys = domainEvent.ModifiedKeys.Count(k => k.Type == ConfigKeyActionType.Set);
             var deletedKeys = domainEvent.ModifiedKeys.Count(k => k.Type == ConfigKeyActionType.Delete);
 
             _logger.LogInformation($"setting '{setKeys}' keys, deleting '{deletedKeys}' keys");
 
-            await _database.ApplyChanges(domainEvent.Identifier,
-                                         domainEvent.ModifiedKeys);
+            var applyChangesResult = await _database.ApplyChanges(domainEvent.Identifier,
+                                                                  domainEvent.ModifiedKeys);
+
+            if (applyChangesResult.IsError)
+                throw new Exception($"failed to apply changes to '{domainEvent.Identifier}': {applyChangesResult.Message}");
 
             _logger.LogInformation($"generating autocomplete-data for environment '{domainEvent.Identifier}'");
 
-            await _database.GenerateEnvironmentKeyAutocompleteData(domainEvent.Identifier);
+            var autoCompleteResult = await _database.GenerateEnvironmentKeyAutocompleteData(domainEvent.Identifier);
+
+            if (autoCompleteResult.IsError)
+                throw new Exception($"failed to generate autocomplete-data after applying changes to environment: '{domainEvent.Identifier}'");
         }
     }
 }
