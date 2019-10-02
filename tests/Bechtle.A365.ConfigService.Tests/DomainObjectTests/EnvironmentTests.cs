8b7e56ea2f32dbc9614a4496d1edc82f195e8255
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-10-02 14:07:04 +0200

    add tests for the missing DomainObjects and focus them on their purpose

---
+++ tests/Bechtle.A365.ConfigService.Tests/DomainObjectTests/EnvironmentTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using App.Metrics;
+using AutoFixture.Xunit2;
+using Bechtle.A365.ConfigService.Common;
+using Bechtle.A365.ConfigService.Common.DomainEvents;
+using Bechtle.A365.ConfigService.DomainObjects;
+using Bechtle.A365.ConfigService.Dto;
+using Xunit;
+
+namespace Bechtle.A365.ConfigService.Tests.DomainObjectTests
+{
+    public class EnvironmentTests
+    {
+        public static IEnumerable<object[]> IdentifiedByParameters() => new[]
+        {
+            new object[] {new EnvironmentIdentifier(string.Empty, string.Empty), false},
+            new object[] {new EnvironmentIdentifier(string.Empty, string.Empty), true},
+            new object[] {new EnvironmentIdentifier("Foo", "Bar"), false},
+            new object[] {new EnvironmentIdentifier("Foo", "Bar"), true},
+            new object[] {new EnvironmentIdentifier(null, null), false},
+            new object[] {new EnvironmentIdentifier(null, null), true},
+            new object[] {new EnvironmentIdentifier(null, string.Empty), true},
+            new object[] {new EnvironmentIdentifier(null, string.Empty), false},
+            new object[] {new EnvironmentIdentifier(string.Empty, null), true},
+            new object[] {new EnvironmentIdentifier(string.Empty, null), false}
+        };
+
+        [Theory]
+        [MemberData(nameof(IdentifiedByParameters))]
+        public void IdentifiedBy(EnvironmentIdentifier identifier, bool isDefault)
+            => new ConfigEnvironment().IdentifiedBy(identifier, isDefault);
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("_")]
+        [InlineData(":")]
+        [InlineData(null)]
+        [InlineData("Foo")]
+        public void DefaultIdentifiedBy(string category)
+            => new ConfigEnvironment().DefaultIdentifiedBy(category);
+
+        [Theory]
+        [AutoData]
+        public void ImportKeys(IEnumerable<DtoConfigKey> keys) => new ConfigEnvironment().ImportKeys(keys);
+
+        [Theory]
+        [AutoData]
+        public void ModifyKeys(IEnumerable<ConfigKeyAction> keys) => new ConfigEnvironment().ModifyKeys(keys);
+
+        [Fact]
+        public void Create() => new ConfigEnvironment().Create();
+
+        [Fact]
+        public void Delete() => new ConfigEnvironment().Delete();
+
+        [Fact]
+        public void ImportNoKeys() => new ConfigEnvironment().ImportKeys(new DtoConfigKey[0]);
+
+        [Fact]
+        public void ImportNullKeys() => new ConfigEnvironment().ImportKeys(null);
+
+        [Fact]
+        public void ModifyNoKeys() => new ConfigEnvironment().ModifyKeys(new ConfigKeyAction[0]);
+
+        [Fact]
+        public void ModifyNullKeys() => new ConfigEnvironment().ModifyKeys(null);
+    }
+}
---
+++ tests/Bechtle.A365.ConfigService.Tests/DomainObjectTests/SnapshotTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Bechtle.A365.ConfigService.Common.DomainEvents;
+using Bechtle.A365.ConfigService.DomainObjects;
+using Xunit;
+
+namespace Bechtle.A365.ConfigService.Tests.DomainObjectTests
+{
+    public class SnapshotTests
+    {
+        public static IEnumerable<object[]> TestDates => new[]
+        {
+            new object[] {DateTime.MinValue},
+            new object[] {DateTime.MaxValue},
+            new object[] {DateTime.UnixEpoch},
+            new object[] {new DateTime()},
+            new object[] {null}
+        };
+
+        [Fact]
+        public void Create() => new ConfigSnapshot().Create();
+
+        [Fact]
+        public void IdentifiedBy() => new ConfigSnapshot().IdentifiedBy(new StructureIdentifier("Foo", 42), new EnvironmentIdentifier("Foo", "Bar"));
+
+        [Theory]
+        [MemberData(nameof(TestDates))]
+        public void ValidFrom(DateTime? dt) => new ConfigSnapshot().ValidFrom(dt);
+
+        [Theory]
+        [MemberData(nameof(TestDates))]
+        public void ValidTo(DateTime? dt) => new ConfigSnapshot().ValidTo(dt);
+    }
+}
