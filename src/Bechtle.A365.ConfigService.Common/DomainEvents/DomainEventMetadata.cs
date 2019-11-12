f91b3e43d6ec9bdd10a27d361960700ed69ab837
sven.degenhardt <sven.degenhardt@bechtle.com>
2019-11-12 08:13:42 +0100

    shorten Filters.get

--- src/Bechtle.A365.ConfigService.Common/DomainEvents/DomainEventMetadata.cs
+++ src/Bechtle.A365.ConfigService.Common/DomainEvents/DomainEventMetadata.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Bechtle.A365.ConfigService.Common.DomainEvents
 {
     public class DomainEventMetadata
     {
-        public Dictionary<string, string> Filters { get; set; }
+        public Dictionary<string, string> Filters { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
         public string this[string index]
         {
-            get
-            {
-                // @TODO: remove var-init when VS finally understands that var is assigned after the condition is evaluated (out var value)
-                var value = string.Empty;
-                return Filters?.TryGetValue(index, out value) == true ? value : string.Empty;
-            }
+            get => Filters.TryGetValue(index, out var value) ? value : string.Empty;
             set => Filters[index] = value;
         }
     }
