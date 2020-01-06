c4caf4b20b6b017b98672f3ddc4448a95e27ad86
sven.degenhardt <sven.degenhardt@bechtle.com>
2020-01-06 13:54:11 +0100

    fix indentation of IConfigurationParser xmldoc

--- src/Bechtle.A365.ConfigService.Parsing/IConfigurationParser.cs
+++ src/Bechtle.A365.ConfigService.Parsing/IConfigurationParser.cs
@@ -8,11 +8,11 @@ namespace Bechtle.A365.ConfigService.Parsing
     ///         Structure: {{Keyword: Value}}
     ///         Allowed Keyword:
     ///         - Using
-    ///         - make the provided value available under the name provided to 'Alias'
-    ///         - must be used in conjunction with 'Alias'
+    ///           - make the provided value available under the name provided to 'Alias'
+    ///           - must be used in conjunction with 'Alias'
     ///         - Alias
     ///         - Path (default)
-    ///         - path to one or more values that should be inserted here
+    ///           - path to one or more values that should be inserted here
     ///     </remarks>
     /// </summary>
     public interface IConfigurationParser
