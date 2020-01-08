7c6b3e878851e5ae33560fa707663d96b1dfe5e0
sven.degenhardt <sven.degenhardt@bechtle.com>
2020-01-08 15:31:14 +0100

    add tests for DomainEventMetadata

---
+++ tests/Bechtle.A365.ConfigService.Tests/Common/DomainEvents/DomainEventMetadataTests.cs
@@ -0,0 +1,38 @@
+﻿using Bechtle.A365.ConfigService.Common.DomainEvents;
+using Xunit;
+
+namespace Bechtle.A365.ConfigService.Tests.Common.DomainEvents
+{
+    public class DomainEventMetadataTests
+    {
+        [Fact]
+        public void AccessUnavailableMetadata()
+        {
+            var metadata = new DomainEventMetadata();
+
+            Assert.Equal(string.Empty, metadata["Foo"]);
+        }
+
+        [Fact]
+        public void CreateMetadata() => Assert.NotNull(new DomainEventMetadata());
+
+        [Fact]
+        public void FillMetadata()
+        {
+            var metadata = new DomainEventMetadata {["Foo"] = "Bar"};
+
+            Assert.Equal("Bar", metadata["Foo"]);
+        }
+
+        [Fact]
+        public void FiltersNotNull() => Assert.NotNull(new DomainEventMetadata().Filters);
+
+        [Fact]
+        public void IndexerCaseInsensitive()
+        {
+            var metadata = new DomainEventMetadata {["Foo"] = "Bar"};
+
+            Assert.Equal("Bar", metadata["foo"]);
+        }
+    }
+}
