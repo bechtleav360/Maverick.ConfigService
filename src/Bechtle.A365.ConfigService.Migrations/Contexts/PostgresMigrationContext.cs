54805383d56792e1b17a7229237493883a2ca7c2
sven.degenhardt <sven.degenhardt@bechtle.com>
2020-02-04 14:39:50 +0100

    remove still existing but unused migrations

---
+++ src/Bechtle.A365.ConfigService.Migrations/Contexts/PostgresMigrationContext.cs
@@ -0,0 +1,13 @@
+﻿using Bechtle.A365.ConfigService.Common.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bechtle.A365.ConfigService.Migrations.Contexts
+{
+    public class PostgresMigrationContext : PostgresSnapshotContext
+    {
+        /// <inheritdoc />
+        public PostgresMigrationContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
