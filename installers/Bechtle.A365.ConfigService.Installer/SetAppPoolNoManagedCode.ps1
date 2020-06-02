697e3d147ec3d83435554041cb55fbe9c9b274d2
sven.degenhardt <sven.degenhardt@bechtle.com>
2020-06-02 10:21:24 +0200

    remove installer from project

--- installers/Bechtle.A365.ConfigService.Installer/SetAppPoolNoManagedCode.ps1
+++
@@ -1,9 +0,0 @@
-﻿param([string]$appPool = "")
-
-if ($appPool -eq "") {
-    Write-Error "application pool undefined, use '-appPool SomeApplicationPool' when running this script";
-    exit 1;
-}
-
-Import-Module WebAdministration;
-Set-ItemProperty IIS:\AppPools\$appPool managedRuntimeVersion "";
