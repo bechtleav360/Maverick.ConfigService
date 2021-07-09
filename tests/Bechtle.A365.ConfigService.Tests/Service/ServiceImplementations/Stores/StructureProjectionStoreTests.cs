f8a6d167982136f9b6aafeb846d2250653a20a87
sven.degenhardt <sven.degenhardt@bechtle.com>
2021-07-09 08:22:12 +0200

    remove tests for functionality that was moved/removed

--- tests/Bechtle.A365.ConfigService.Tests/Service/ServiceImplementations/Stores/StructureProjectionStoreTests.cs
+++ tests/Bechtle.A365.ConfigService.Tests/Service/ServiceImplementations/Stores/StructureProjectionStoreTests.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bechtle.A365.ConfigService.Common;
 using Bechtle.A365.ConfigService.Common.DomainEvents;
-using Bechtle.A365.ConfigService.DomainObjects;
 using Bechtle.A365.ConfigService.Implementations.Stores;
 using Bechtle.A365.ConfigService.Interfaces;
-using Bechtle.A365.ConfigService.Interfaces.Stores;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
